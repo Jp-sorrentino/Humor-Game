3ac8be9eb3ccb83c4422d92075d6f52425dd6bc8
João <jp.sorrentin@gmail.com>
2021-11-01 19:36:42 -0300

    dia 5

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let elementBtn = document.getElementById('btn') /* Cria a variável elementBtn e coloca o element com o id alterar nela */
+
+
+const elementName = document.getElementById('Character'); 
+const elementHumor = document.getElementById('Humor');
+const elementImg = document.getElementsByClassName('box2') Style.backgroundImage
+const 
+
+elementBtn.addEventListener('click', () =>{ 
+   
+    if(elementBtn.value == 'one'){
+        elementName.innerText = 'Knuckles the Echidna'
+        elementHumor.innerText = 'He is Red'
+        elementBtn.value = 'two'
+        elementImg.style.backgroundImage = url('../images/img2.png') 
+        
+    } else if(elementBtn.value == "two") { 
+        elementName.innerText = 'Tails the Fox' 
+        elementHumor.innerText = 'He is yellow' 
+        elementBtn.value = 'tree' 
+        elementImg.style.backgroundImage = url('../images/img3.png') 
+
+    } else if(elementBtn.value == "tree") {
+        elementName.innerText = 'Sonic the Hedgehog'
+        elementHumor.innerText = 'He is Blue' 
+        elementBtn.value = 'one' }
+        elementImg.style.backgroundImage = url('../images/img.png') 
+})

8037d438bf3f4fe20c838da85b6068025812e19b
João <jp.sorrentin@gmail.com>
2021-11-03 19:10:31 -0300

    dia 6

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let elementBtn = document.getElementById('btn') /* Cria a variável elementBtn e
 
 const elementName = document.getElementById('Character'); 
 const elementHumor = document.getElementById('Humor');
-const elementImg = document.getElementsByClassName('box2') 
+const elementImg = document.getElementById('box2') 
 
 elementBtn.addEventListener('click', () =>{ 
    
@@ -11,17 +11,17 @@ elementBtn.addEventListener('click', () =>{
         elementName.innerText = 'Knuckles the Echidna'
         elementHumor.innerText = 'He is Red'
         elementBtn.value = 'two'
-        elementImg.style.backgroundImage = url('../images/img2.png') 
+        elementImg.style.backgroundImage = "url('../images/img3.png')";
 
     } else if(elementBtn.value == "two") { 
         elementName.innerText = 'Tails the Fox' 
         elementHumor.innerText = 'He is yellow' 
         elementBtn.value = 'tree' 
-        elementImg.style.backgroundImage = url('../images/img3.png') 
+        elementImg.style.backgroundImage = "url('../images/img2.png')";
 
     } else if(elementBtn.value == "tree") {
         elementName.innerText = 'Sonic the Hedgehog'
         elementHumor.innerText = 'He is Blue' 
         elementBtn.value = 'one' }
-        elementImg.style.backgroundImage = url('../images/img.png') 
+        elementImg.style.backgroundImage = "url('../images/img.png')";
 })
